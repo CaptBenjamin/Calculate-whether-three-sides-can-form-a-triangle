b10f3d177b8fba8a6a11f1fe4507a21ed1004dc4
CaptB <63907030+CaptBenjamin@users.noreply.github.com>
2022-09-18 18:29:14 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("请输入一个值给a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入一个值给b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入一个值给c");
+            c = Convert.ToDouble(Console.ReadLine());
+            if (a + b <= c || a + c <= b || b + c <= a) Console.WriteLine("不构成三角形");
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                Console.WriteLine("可以构成三角形");
+                if (a == b && b == c) Console.WriteLine("构成等边三角形");
+                if (a == b || a == c || b == c) Console.WriteLine("构成等腰三角形");
+                if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) Console.WriteLine("构成直角三角形");
+            }
+            Console.Read();
+        }
+    }
+}
